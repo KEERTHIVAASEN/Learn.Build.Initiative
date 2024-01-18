05ae3a8683b9cad249d77c918a267603c5ed2e63
KEERTHIVAASEN V <84181432+KEERTHIVAASEN@users.noreply.github.com>
2024-01-18 15:29:32 +0000

    Add Blog model and CRUD operations
    This commit adds the Blog model to the auth/models.py file and includes CRUD operations for creating, retrieving, updating, and deleting blog posts.

--- auth/models.py
+++ auth/models.py
@@ -1,8 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from djongo import models
+from .models import Blog
 
 class User(AbstractUser):
     # additional fields in here
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(null=True)
+    
+
+def create_blog(request):
+    blog = Blog(title='My first blog', content='This is my first blog post.')
+    blog.save()
+
+def get_all_blogs(request):
+        blogs = Blog.objects.all()  # retrieves all blogs
+
+def get_single_blog(request, blog_id):
+        blog = Blog.objects.get(id=blog_id)  # retrieves a single blog by its id
+
+def update_blog(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.title = 'Updated title'
+    blog.content = 'Updated content'
+    blog.save()
+
+def delete_blog(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.delete()    
